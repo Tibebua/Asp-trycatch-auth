42284905c881ceac3add87d4baa94fece46f2d83
Alazar <tibebualazar@gmail.com>
2022-05-11 18:45:02 +0300

    Add project files.

---
+++ asp-trycatch-auth/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using asp_trycatch_auth.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace asp_trycatch_auth.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
